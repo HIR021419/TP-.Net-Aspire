bcc817192d3d228d8870ab2b6d761639b69c1e5c
HIR021419 <yann.lemetayer@epita.fr>
2025-10-14 23:56:59 +0200

    feat: deployed on kube

--- MicroServices.Aspire/TodoService/Models/NewTodo.cs
+++ MicroServices.Aspire/TodoService/Models/NewTodo.cs
@@ -3,7 +3,7 @@
     public class NewTodo
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public bool IsDone { get; set; }
 
         public int UserId { get; set; }
--- MicroServices.Aspire/TodoService/Models/Todo.cs
+++ MicroServices.Aspire/TodoService/Models/Todo.cs
@@ -19,11 +19,11 @@ namespace TodoService.Models
         }
 
         public Guid Id { get; set; } 
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public bool IsDone { get; set; }
 
         public int UserId { get; set; }
-        
-        public User User { get; set; }
+
+        public User User { get; set; } = null!;
     }
 }
